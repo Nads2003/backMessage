415f660ad12390594734fb11c969f9402bd6a43f
sambatra <fitivanjanaharysambatriniaina@gmail.com>
2026-01-14 03:28:18 +0300

    base automatique

--- backend/asgi.py
+++ backend/asgi.py
@@ -3,6 +3,16 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import JWTAuthMiddleware
 import chat.routing
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+if os.environ.get("ENVIRONMENT") == "production":
+    try:
+        from django.core.management import call_command
+        call_command("migrate", interactive=False)
+    except Exception as e:
+        print("Migration error:", e)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
